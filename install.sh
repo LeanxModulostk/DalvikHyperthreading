4656b073ee3cc55532d7d8a6f4f8a4297e6dbbc3
@LeanHijosdesusMadres <105339980+LeanxModulostk@users.noreply.github.com>
2023-01-14 15:20:27 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,51 @@
+SKIPMOUNT=false
+PROPFILE=true
+POSTFSDATA=false
+LATESTARTSERVICE=false
+
+##########################################################################################
+# Replace list
+##########################################################################################
+
+# List all directories you want to directly replace in the system
+# Check the documentations for more info why you would need this
+
+# Construct your list in the following format
+# This is an example
+REPLACE_EXAMPLE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
+# Construct your own list here
+REPLACE="
+"
+
+# Set what you want to display when installing your module
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "     DalvikHyperthreading     "
+  ui_print "       [t.me/modulostk]       "  
+  ui_print "*******************************"
+}
+
+# Copy/extract your module files into $MODPATH in on_install.
+
+on_install() {
+  # The following is the default implementation: extract $ZIPFILE/system to $MODPATH
+  # Extend/change the logic to whatever you want
+  ui_print "- Extracting module files"
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+}
+
+# Only some special files require specific permissions
+# This function will be called after on_install is done
+# The default permissions should be good enough for most cases
+
+set_permissions() {
+  # The following is the default rule, DO NOT remove
+  set_perm_recursive $MODPATH 0 0 0755 0644
+}
